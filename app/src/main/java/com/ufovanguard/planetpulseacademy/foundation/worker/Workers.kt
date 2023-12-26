3d5e48fa0d23a90d70893e37d745b48064858198
kafri8889 <kafri8889@gmail.com>
2023-12-26 11:02:00 +0700

    Setup worker

---
+++ app/src/main/java/com/ufovanguard/planetpulseacademy/foundation/worker/Workers.kt
@@ -0,0 +1,4 @@
+package com.ufovanguard.planetpulseacademy.foundation.worker
+
+object Workers {
+}
---
+++ app/src/main/java/com/ufovanguard/planetpulseacademy/foundation/worker/di/WorkerModule.kt
@@ -0,0 +1,22 @@
+package com.ufovanguard.planetpulseacademy.foundation.worker.di
+
+import android.content.Context
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class WorkerModule {
+
+	@Provides
+	@Singleton
+	fun provideWorkManager(
+		@ApplicationContext context: Context
+	): WorkManager = WorkManager.getInstance(context)
+
+}
